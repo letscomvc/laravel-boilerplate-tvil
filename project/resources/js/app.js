ae2845df3205d415e12d7265797db47e852d8c2a
Danilo Pinotti <danilopinotti@hotmail.com>
2020-11-30 12:26:15 -0300

    Configurar dynamic imports

--- project/resources/js/app.js
+++ project/resources/js/app.js
@@ -20,7 +20,7 @@ new Vue({
         h(InertiaApp, {
             props: {
                 initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
+                resolveComponent: (name) => import(`./Pages/${name}`).then(module => module.default),
             },
         }),
 }).$mount(app);
