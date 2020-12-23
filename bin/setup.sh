6dd7574ca2e5272ecfd78d1d14ae72e9b21c3909
Danilo Pinotti <danilopinotti@hotmail.com>
2020-12-23 17:02:48 -0300

    Adicionar storage:link no script de configuração do projeto

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,6 +6,7 @@ docker-compose exec app cp .env.testing.example .env.testing
 docker-compose exec app composer install
 docker-compose exec app php artisan migrate:fresh --seed
 docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan storage:link
 
 docker-compose exec app npm install
 docker-compose exec app npm run dev
